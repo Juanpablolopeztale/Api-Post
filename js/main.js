3cb9a480dc2cc7dd654f1b541c07cecab2632e39
Pablo <jplopez@scl.edu.gt>
2023-06-07 21:33:00 -0600

    Perfil

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+fetch('https://jsonplaceholder.typicode.com/users/4') // Change the URL to fetch the desired user profile
+  .then(response => response.json())
+  .then(profileData => {
+    const profileContainer = document.getElementById('profile-container');
+
+    const profileDiv = document.createElement('div');
+    profileDiv.className = 'profile';
+
+    const profileName = document.createElement('h2');
+    profileName.className = 'profile-name';
+    profileName.textContent = profileData.name;
+
+    const profileEmail = document.createElement('p');
+    profileEmail.className = 'profile-email';
+    profileEmail.textContent = profileData.email;
+
+    profileDiv.appendChild(profileName);
+    profileDiv.appendChild(profileEmail);
+
+    profileContainer.appendChild(profileDiv);
+  });
+
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(data => {
